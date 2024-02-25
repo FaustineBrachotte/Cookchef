25bc3a716b3ba5fb14051807790fd4d09a57672a
Faustine Brachotte <faustine.brachotte@gmail.com>
2024-02-25 18:02:43 +0100

    ajout favorite recipes

--- src/components/Recipe.jsx
+++ src/components/Recipe.jsx
@@ -1,14 +1,22 @@
 import styles from './Recipe.module.scss'
-import recipe from '../assets/images/recette.jpg'
+import { useState } from 'react';
 
-function Recipe() {
+function Recipe({title, image}) {
+
+  const [liked, setLiked] = useState(false);
+
+  function handleClick() {
+    setLiked(!liked);
+  }
+
     return (
-      <div className={styles.recipe}>
+      <div onClick={handleClick} className={styles.recipe}>
         <div className={styles.imageContainer}>
-            <img src={recipe} alt="Recette"/>
+            <img src={image} alt="Recette"/>
         </div>
-        <div className={` ${styles.recipeTitle} d-flex flex-row justify-content align-items`}>
-            <h3>Spaghettis à la bolognaise</h3>
+        <div className={` ${styles.recipeTitle} d-flex flex-column justify-content align-items`}>
+            <h3 className="mb-10">{title}</h3>
+            <i className={` fa-solid fa-heart ${ liked ? 'text-primary' : ''} `}></i>
         </div>
         
       </div>
