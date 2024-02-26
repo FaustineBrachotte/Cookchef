07075738cdee3880325c35a5612a59fe43ae1b49
Faustine Brachotte <faustine.brachotte@gmail.com>
2024-02-26 01:31:53 +0100

    ajout menu mobile

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,29 @@
 import styles from './Header.module.scss'
 import logo from '../assets/images/cookchef.png'
+import { useState } from 'react'
+import HeaderMenu from './HeaderMenu';
 
 function Header() {
+  const [showMenu, setShowMenu] = useState(false);
+
     return (
       <header className={` ${styles.header} d-flex flex-row align-items`}>
-        <i className="fa-solid fa-bars mr-15"></i>
         <div className="flex-fill">
             <img src={logo} alt="Logo Cookchef"/>
         </div>
-        <ul>
+        <ul className={styles.headerList}>
             <button className="mr-5 btn btn-reverse-primary">
-                <i className="fa-solid fa-basket-shopping mr-5"></i>
-                <span>Panier</span>
+                <i className="fa-solid fa-heart mr-5"></i>
+                <span>Wishlist</span>
             </button>
             <button className="btn btn-primary">Connexion</button>
         </ul>
+        <i onClick={ () => setShowMenu(true) } className={` ${styles.headerXS} fa-solid fa-bars`}></i>
+        { showMenu && 
+          <>
+            <div onClick={ () => setShowMenu(false) }className="calc"></div>
+            <HeaderMenu/>
+          </> }
       </header>
     )
   }
