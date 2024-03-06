0bcc1fd10a6841b3adad93c70ce286950706c1a8
Faustine Brachotte <faustine.brachotte@gmail.com>
2024-03-06 13:47:49 +0100

    Ajout formulaire

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,21 @@
+import { useState } from 'react';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 import Homepage from './pages/Homepage/Homepage';
+import Admin from './pages/Homepage/Admin/Admin';
 import styles from './App.module.scss';
 // import { seedRecipes } from './data/seed';
 
 // seedRecipes();
 
 function App() {
+	const [page, setPage] = useState('homepage');
+
 	return (
 		<div className={`d-flex flex-column ${styles.appContainer}`}>
-			<Header />
-			<Homepage />
+			<Header setPage={setPage} />
+			{page === 'homepage' && <Homepage />}
+			{page === 'admin' && <Admin />}
 			<Footer />
 		</div>
 	);
